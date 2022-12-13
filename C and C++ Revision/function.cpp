82464d29973cf72188f2b81ab090e91fb2972c36
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-13 10:11:06 +0600

    Normal function added

---
+++ C and C++ Revision/function.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int add(int x, int y){
+	return x + y;
+}
+
+int main(){
+	int sum;
+	sum = add(56, 45);
+	cout<<sum<<endl;
+	return 0;
+}
