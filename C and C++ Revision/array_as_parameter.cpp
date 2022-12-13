7b7d06e1659d63b0e7abe21e95cd77b7814c660c
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-13 14:44:20 +0600

    returning array

--- C and C++ Revision/array_as_parameter.cpp
+++ C and C++ Revision/array_as_parameter.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int N = 0;
 
 //void print_array(int *arr, int n){	// Using (*) operator
-void print_array(int arr[], int n){
+void print_array(int arr[], int n){		// Using []
 	for(int i = 0; i < n; i ++){
 		cout<<arr[i]<<" ";
 	}
@@ -15,7 +15,8 @@ int main(){
 	int arr[] = {3, 4, 6, 2, 8, 4};
 	int size = sizeof(arr)/sizeof(arr[0]);
 
-	print_array(arr, size);
+	print_array(arr, size);		//Here the address of arr will be passed.
+	
 
 	return 0;
 }
---
+++ C and C++ Revision/returning_array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int *get_array(int size){
+	int *arr;
+	//arr = (int*)malloc(sizeof(size));	// Allocating memory into heap by using c
+	arr = new int(size);		// Allocating memory into heap by using c++
+
+	for(int i = 0; i < size; i++){
+		cin>>arr[i];
+	}
+
+	return arr;
+}
+
+int main(){
+	
+	int *ptr;
+	int n;
+	cin>>n;
+	ptr = get_array(n);
+
+	for(int i = 0; i < n; i++){
+		cout<<ptr[i]<<" ";
+	}
+
+	delete []ptr;	//Free the heap memory in c++
+	//free(ptr);		//Free the memory in c;
+
+	return 0;
+}
