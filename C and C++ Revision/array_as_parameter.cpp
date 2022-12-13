7a868a380eecb210c174988b3995880d4ff0946d
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-13 12:57:46 +0600

    passing array as parameter

---
+++ C and C++ Revision/array_as_parameter.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+//void print_array(int *arr, int n){	// Using (*) operator
+void print_array(int arr[], int n){
+	for(int i = 0; i < n; i ++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main(){
+	
+	int arr[] = {3, 4, 6, 2, 8, 4};
+	int size = sizeof(arr)/sizeof(arr[0]);
+
+	print_array(arr, size);
+
+	return 0;
+}
