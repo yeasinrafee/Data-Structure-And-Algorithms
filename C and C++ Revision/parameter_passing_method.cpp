741a35952353f278bf8d05351e005807c267d4de
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-13 10:44:47 +0600

    passing by address

--- C and C++ Revision/parameter_passing_method.cpp
+++ C and C++ Revision/parameter_passing_method.cpp
@@ -4,17 +4,26 @@ using namespace std;
 const int N = 0;
 
 //pass by value:
-void swap(int a, int b){
+void swap1(int a, int b){
 	int temp;
 	temp = a;
 	a = b;
 	b = temp;
 }
 
+//pass by address:
+void swap2(int *a, int *b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 int main(){
 	
 	int num1 = 1, num2 = 9;
-	swap(num1, num2);	//It'll not work, for passing by value
+	// swap1(num1, num2);	//It'll not work, for passing by value
+	swap2(&num1, &num2);
 
 	cout<<"Number-1: "<<num1<<endl<<"Number-2: "<<num2<<endl;
 
