70dd1cc25be09d7342d82acc9033b37856a269a7
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-12-31 01:58:44 +0600

    Fibonacci recursive (memoization)

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 const int N = 0;
 
+int F[10];
+
+//Using iterative way:
 int fib(int n){
 	int t0 = 0, t1 = 1, s = 0;
 
@@ -17,14 +20,35 @@ int fib(int n){
 	return s;
 }
 
+//Using normal recursive way:
 int rfib(int n){
 	if(n <= 1) return n;
 	return rfib(n - 2) + rfib(n - 1);
 }
 
+
+//Using memoization recursive way:
+int mfib(int n){
+	if(n <= 1){
+		F[n] = n;
+		return n;
+	}else{
+		if(F[n-2] == -1)
+			F[n-2] = mfib(n-2);
+		if(F[n-1] == -1)
+			F[n-1] = mfib(n-1);
+
+		return F[n-2] + F[n-1];
+	}
+}
+
 int main(){
+
+	for(int i = 0; i < 10; i++){
+		F[i] = -1;
+	}
 	
-	cout<<rfib(6)<<endl;
+	cout<<mfib(6)<<endl;
 
 	return 0;
 }
