058618d5952a640c09dc1079870dee1811807596
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-01-03 01:19:27 +0600

    Array representation

---
+++ 03. Array Representation/array_declaration.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Array declaration:
+int main(){
+	
+	int A[5];
+	int B[5] = {1, 2, 3, 4, 5};
+	int C[10] = {2, 4, 6};
+	int D[5] = {0};
+	int E[] = {1, 2, 3, 4, 5, 6, 7};
+	
+	return 0;
+}
+
+
